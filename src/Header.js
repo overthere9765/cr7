1876fcd8ff09ac744b72ddef663cc89734039094
Simon <simon72217@gmail.com>
2022-12-10 17:41:09 +0700

    a

--- src/Header.js
+++ src/Header.js
@@ -5,52 +5,146 @@ import Wallet from "./com/Wallet";
 class Header extends React.Component {
     render() {
         return (
-            <><header className="header">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12">
-                            <div className="header__content">
-                                <button className="header__btn" type="button">
-                                    <span></span>
-                                    <span></span>
-                                    <span></span>
+            <div class="header_section">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <div class="logo"><a href="#"><img src="images/logo.png" /></a></div>
+                        </div>
+                        <div class="col-sm-9">
+                            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                                    data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                                    aria-label="Toggle navigation">
+                                    <span class="navbar-toggler-icon"></span>
                                 </button>
-
-                                <a data-scroll href="#home" className="header__logo">
-                                    <img className="header__logo-white" src="img/logo.png" alt="Brazil Fan Club" />
-                                    <img className="header__logo-dark" src="img/logo.png" alt="Brazil Fan Club" />
-                                </a>
-
-                                <span className="header__tagline">Brazil<br />Fan Club</span>
-
-                                <ul className="header__nav">
-                                    <li className="dropdown header__dropdown">
-                                        <a data-scroll href="#home">Home
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a data-scroll href="#about">About</a>
-                                    </li>
-                                    <li>
-                                        <a data-scroll href="#getstarted">Get Started</a>
-                                    </li>
-                                    <li>
-                                        <a data-scroll href="#features">Features</a>
-                                    </li>
-                                    <li>
-                                        <a data-scroll href="#safety">Safety</a>
-                                    </li>
-                                    <li>
-                                        <a data-scroll href="#advisors">Advisors</a>
-                                    </li>
-                                </ul>
-                                <Wallet />
-                                {/* <a href="#signin" className="header__signin modal-btn"><span>Sign In</span></a> */}
-                            </div>
+                                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                                    <div class="navbar-nav">
+                                        <a class="nav-item nav-link" href="index.html">Home</a>
+                                        <a class="nav-item nav-link" href="collection.html">Collection</a>
+                                        <a class="nav-item nav-link" href="shoes.html">Shoes</a>
+                                        <a class="nav-item nav-link" href="racing boots.html">Racing Boots</a>
+                                        <a class="nav-item nav-link" href="contact.html">Contact</a>
+                                        <a class="nav-item nav-link last" href="#"><img src="images/search_icon.png" /></a>
+                                        <a class="nav-item nav-link last" href="contact.html"><img src="images/shop_icon.png" /></a>
+                                    </div>
+                                </div>
+                            </nav>
                         </div>
                     </div>
                 </div>
-            </header></>
+                <div class="banner_section">
+                    <div class="container-fluid">
+                        <section class="slide-wrapper">
+                            <div class="container-fluid">
+                                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                                    <ol class="carousel-indicators">
+                                        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                                        <li data-target="#myCarousel" data-slide-to="1"></li>
+                                        <li data-target="#myCarousel" data-slide-to="2"></li>
+                                        <li data-target="#myCarousel" data-slide-to="3"></li>
+                                    </ol>
+
+                                    <div class="carousel-inner">
+                                        <div class="carousel-item active">
+                                            <div class="row">
+                                                <div class="col-sm-2 padding_0">
+                                                    <p class="mens_taital">Men Shoes</p>
+                                                    <div class="page_no">0/2</div>
+                                                    <p class="mens_taital_2">Men Shoes</p>
+                                                </div>
+                                                <div class="col-sm-5">
+                                                    <div class="banner_taital">
+                                                        <h1 class="banner_text">New Running Shoes </h1>
+                                                        <h1 class="mens_text"><strong>Men's Like Plex</strong></h1>
+                                                        <p class="lorem_text">ipsum dolor sit amet, consectetur adipiscing elit,
+                                                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                                        </p>
+                                                        <button class="buy_bt">Buy Now</button>
+                                                        <button class="more_bt">See More</button>
+                                                    </div>
+                                                </div>
+                                                <div class="col-sm-5">
+                                                    <div class="shoes_img"><img src="images/running-shoes.png" /></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="carousel-item">
+                                            <div class="row">
+                                                <div class="col-sm-2 padding_0">
+                                                    <p class="mens_taital">Men Shoes</p>
+                                                    <div class="page_no">0/2</div>
+                                                    <p class="mens_taital_2">Men Shoes</p>
+                                                </div>
+                                                <div class="col-sm-5">
+                                                    <div class="banner_taital">
+                                                        <h1 class="banner_text">New Running Shoes </h1>
+                                                        <h1 class="mens_text"><strong>Men's Like Plex</strong></h1>
+                                                        <p class="lorem_text">ipsum dolor sit amet, consectetur adipiscing elit,
+                                                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                                        </p>
+                                                        <button class="buy_bt">Buy Now</button>
+                                                        <button class="more_bt">See More</button>
+                                                    </div>
+                                                </div>
+                                                <div class="col-sm-5">
+                                                    <div class="shoes_img"><img src="images/running-shoes.png" /></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="carousel-item">
+                                            <div class="row">
+                                                <div class="col-sm-2 padding_0">
+                                                    <p class="mens_taital">Men Shoes</p>
+                                                    <div class="page_no">0/2</div>
+                                                    <p class="mens_taital_2">Men Shoes</p>
+                                                </div>
+                                                <div class="col-sm-5">
+                                                    <div class="banner_taital">
+                                                        <h1 class="banner_text">New Running Shoes </h1>
+                                                        <h1 class="mens_text"><strong>Men's Like Plex</strong></h1>
+                                                        <p class="lorem_text">ipsum dolor sit amet, consectetur adipiscing elit,
+                                                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                                        </p>
+                                                        <button class="buy_bt">Buy Now</button>
+                                                        <button class="more_bt">See More</button>
+                                                    </div>
+                                                </div>
+                                                <div class="col-sm-5">
+                                                    <div class="shoes_img"><img src="images/running-shoes.png" /></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="carousel-item">
+                                            <div class="row">
+                                                <div class="col-sm-2 padding_0">
+                                                    <p class="mens_taital">Men Shoes</p>
+                                                    <div class="page_no">0/2</div>
+                                                    <p class="mens_taital_2">Men Shoes</p>
+                                                </div>
+                                                <div class="col-sm-5">
+                                                    <div class="banner_taital">
+                                                        <h1 class="banner_text">New Running Shoes </h1>
+                                                        <h1 class="mens_text"><strong>Men's Like Plex</strong></h1>
+                                                        <p class="lorem_text">ipsum dolor sit amet, consectetur adipiscing elit,
+                                                            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                                        </p>
+                                                        <button class="buy_bt">Buy Now</button>
+                                                        <button class="more_bt">See More</button>
+                                                    </div>
+                                                </div>
+                                                <div class="col-sm-5">
+                                                    <div class="shoes_img"><img src="images/running-shoes.png" /></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
---
+++ src/com/OneCollection.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+
+class OneCollection extends React.Component {
+    render() {
+        return (
+            <div class="collection_section">
+                <div class="container">
+                    <h1 class="new_text"><strong>Racing Boots</strong></h1>
+                    <p class="consectetur_text">consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default OneCollection;
